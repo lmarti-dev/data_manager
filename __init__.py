9a1b4dd8fb64a0077fc2b51700fabc359f886498
Lucas <lucas.marti@dxdt.ch>
2024-02-01 11:33:31 +0100

    fau colors

--- __init__.py
+++ __init__.py
@@ -1,7 +1,12 @@
-from data_manager import ExperimentDataManager, print_experiments_without_data  # noqa
+from data_manager import (
+    ExperimentDataManager,
+    print_experiments_without_data,
+    set_color_cycler,
+)  # noqa
 from json_extender import ExtendedJSONDecoder, ExtendedJSONEncoder  # noqa
 
 __all__ = [
+    "set_color_cycler",
     "ExperimentDataManager",
     "print_experiments_without_data",
     "ExtendedJSONDecoder",
